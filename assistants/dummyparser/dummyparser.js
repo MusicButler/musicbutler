d1c9d0e380d86fa35a7ccf6239003c921c1b8e69
Paul Varache <perso@paulvarache.ninja>
2014-10-12 19:52:08 +0000

    Added client and new assistant manager

--- assistants/dummyparser/dummyparser.js
+++
@@ -1,8 +0,0 @@
-function parser (input) {
-    console.log('dummy');
-    return input;
-}
-module.exports = function (player, done) {
-    player.parsers.push({order: 1, func: parser});
-    done({name: "Dummy parser"});
-}
